158ae55c3b1b51b4a43fe556957de6deaba26d6e
rolpppp <rlgarces@up.edu.ph>
2026-07-01 22:39:26 +0800

    feat: update layoour

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://rolpppp.github.io',
+  // Uncomment and set to your repo name when deploying as a project site:
+  // base: '/portfolio-website',
+  output: 'static',
+});
